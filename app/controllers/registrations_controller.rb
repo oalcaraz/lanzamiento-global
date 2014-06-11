20c7b3f276f5f4abcbcbda585bd3c992d9ae3a28
Nahuel Becerra <nahuel.becerra97@gmail.com>
2014-06-11 13:29:44 -0300

    Finished refer_id

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
    def new
-      :refer_id = Account.find(:params[:username] == @account.username, :select => 'id')
+      refer = Account.find_by(username: params[:username])
+      refer = Account.all.first unless refer.present? # In case of a kind of hack
 
       plan = Plan.where(name: params[:plan]).first
-      plan = Plan.first unless @plan_id.present? # In case of a kind of hack
+      plan = Plan.all.first unless plan.present? # In case of a kind of hack
 
-      build_resource({plan_id: plan.id})
+      build_resource({plan_id: plan.id, refer_id: refer.id})
       respond_with self.resource
     end
 
