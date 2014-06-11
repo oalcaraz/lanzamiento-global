98c724477bd1bb87e456f472a9c58f3ed745f7c8
Nahuel Becerra <nahuel.becerra97@gmail.com>
2014-06-11 18:45:02 -0300

    Added 3 parents

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
 	def index
 		@children = Account.where(refer: current_account)
 		@parent = Account.find(current_account.refer.id)
+		@parent2 = Account.find(@parent.refer.id)
+		@parent3 = Account.find(@parent2.refer.id)
 	end
 	
 end
