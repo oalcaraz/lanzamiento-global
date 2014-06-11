d31e4183ed8b7dfb59238794f84f74697f0ed07e
Nahuel Becerra <nahuel.becerra97@gmail.com>
2014-06-11 18:40:43 -0300

    Parent and children in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
 
 	def index
-
+		@children = Account.where(refer: current_account)
+		@parent = Account.find(current_account.refer.id)
 	end
 	
 end
