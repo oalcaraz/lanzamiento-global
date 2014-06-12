df6439c40251ec893f8b7f4ab10cbb11ac735c5d
Nahuel Becerra <nahuel.becerra97@gmail.com>
2014-06-12 13:38:02 -0300

    Fixed register css

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
-   def new
-      refer = Account.find_by(username: params[:username])
-      refer = Account.all.first unless refer.present? # In case of a kind of hack
+  def new
+    refer = Account.find_by(username: params[:username])
+    refer = Account.all.first unless refer.present? # In case of a kind of hack
 
-      plan = Plan.where(name: params[:plan]).first
-      plan = Plan.all.first unless plan.present? # In case of a kind of hack
+    plan = Plan.where(name: params[:plan]).first
+    plan = Plan.all.first unless plan.present? # In case of a kind of hack
 
-      build_resource({plan_id: plan.id, refer_id: refer.id})
-      respond_with self.resource
-    end
+    build_resource({plan_id: plan.id, refer_id: refer.id})
+    respond_with self.resource, layout: 'home'
+  end
 
 end
